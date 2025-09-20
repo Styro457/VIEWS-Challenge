ae99be93326082f5de83cd627352d732d4479f6c
Styro <69657780+Styro457@users.noreply.github.com>
2025-09-20 16:22:58 +0300

    Add gzip compression to FastAPI app

--- src/views_challenge/main.py
+++ src/views_challenge/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-
+from starlette.middleware.gzip import GZipMiddleware
 from views_challenge.api import api
 
 app = FastAPI()
 app.include_router(api.router)
+
+app.add_middleware(GZipMiddleware, minimum_size=1024)
+
