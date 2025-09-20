b42fdfaf8740acc3ba997de584d452da3bfc2861
Styro <69657780+Styro457@users.noreply.github.com>
2025-09-20 21:24:12 +0300

    Load country_names in memory

--- src/views_challenge/utils/utils.py
+++ src/views_challenge/utils/utils.py
@@ -1,25 +1,21 @@
 """
 Shared utility functions for the VIEWS Challenge application.
 """
-
 import json
-import os
+from pathlib import Path
 from typing import Optional
 
+def _load_countries() -> dict[int, str]:
+    """
+    Returns a dictionary with the key being the code as an integer and value the country name.
+    """
+    path = Path(__file__).parent / "m49-list.json"
+    with open(path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        return {int(c["m49code"]): c["name"] for c in data["countries"]}
+
+country_names = _load_countries()
 
 def decode_country(country_id: int) -> Optional[str]:
     """Returns country name based on M49 ID"""
-    try:
-        # Path to country mapping file (in api directory)
-        country_list_path = os.path.join(
-            os.path.dirname(__file__), "api", "m49-list.json"
-        )
-        with open(country_list_path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            countries_list = data["countries"]
-            for country in countries_list:
-                if int(country["m49code"]) == country_id:
-                    return country["name"]
-    except (FileNotFoundError, KeyError, ValueError):
-        pass
-    return None
+    return country_names.get(country_id)
