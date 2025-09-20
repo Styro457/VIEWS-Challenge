48041fdd176451d6a7e5d3da0ec9c2b74a708d59
Caleb <orangasus@gmail.com>
2025-09-20 14:47:35 +0300

    added decode c ountry by id function and updated query parameters for return to user

--- src/views_challenge/api/api.py
+++ src/views_challenge/api/api.py
@@ -1,17 +1,53 @@
+import json
+import os
 from enum import Enum
 from typing import List
 
 import pandas
 from fastapi import APIRouter, Query
 
+country_list_path = "./m49-list.json"
+# pandas reads the file relative to location from where the program is executed
+parquet_filepath = os.path.join(os.path.dirname(__file__), "preds_001.parquet")
+print(parquet_filepath)
+
 
 class ViolenceType(str, Enum):
-    os = "os",
-    ns = "ns",
+    os = "os"
+    ns = "ns"
     sb = "sb"
 
 
-filepath = "./preds_001.parquet"
+class ReturnParameters(str, Enum):
+    grid_id = "grid_id"
+    lat_lon = "lat_lon"
+    country_id = "country_id"
+    map_value = "map_value"
+    ci_50 = "ci_50"
+    ci_90 = "ci_90"
+    ci_99 = "ci_99"
+    prob_above_10 = "prob_above_10"
+    prob_above_20 = "prob_above_20"
+    prob_above_30 = "prob_above_30"
+    prob_above_40 = "prob_above_40"
+    prob_above_50 = "prob_above_50"
+    prob_above_60 = "prob_above_60"
+
+
+def decode_country(country_id):
+    """Returns country name based on M49 ID"""
+    with open(country_list_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        countries_list = data["countries"]
+        for country in countries_list:
+            if int(country["m49code"]) == country_id:
+                return country["name"]
+        return None
+
+
+def decode_month(month_id):
+    return
+
 
 router = APIRouter()
 
@@ -19,7 +55,7 @@ router = APIRouter()
 @router.get(path="/all_months")
 def get_all_months():
     """Returns all available months"""
-    df = pandas.read_parquet(filepath, engine="pyarrow")
+    df = pandas.read_parquet(parquet_filepath, engine="pyarrow")
     df = df.reset_index()
     unique_months = df['month_id'].unique()
     return unique_months.tolist()
@@ -28,7 +64,7 @@ def get_all_months():
 @router.get(path="/all_cells")
 def get_all_cells():
     """Returns all available cells"""
-    df = pandas.read_parquet(filepath, engine="pyarrow")
+    df = pandas.read_parquet(parquet_filepath, engine="pyarrow")
     df = df.reset_index()
     unique_cells = df['priogrid_id'].unique()
     return unique_cells.tolist()
@@ -52,13 +88,13 @@ def filter_file(df, priogrid_ids, month_range_start, month_range_end, country_id
 
 @router.get(path="/cells")
 def get_cells_by(ids: List[int] = Query(None, description="List of Cells IDs"), month_range_start: int = None,
-                 month_range_end: int = None, country_id: int = None, violence_type: ViolenceType = Query(None)):
+                 month_range_end: int = None, country_id: int = None, violence_type: ViolenceType = Query(None),
+                 return_param_selection: List[ReturnParameters] = Query(...)):
     """Returns all cells that match the criteria passed as query parameters"""
     print(
-        f"IDs: {ids}\nMonth Range:{month_range_start} - {month_range_end}\nCountry: {country_id}\nViolence Type: {violence_type}")
-    df = pandas.read_parquet(filepath, engine="pyarrow")
+        f"IDs: {ids}\nMonth Range:{month_range_start} - {month_range_end}\nCountry: {country_id}\nViolence Type: {violence_type}\nParams requested: {return_param_selection}")
+    df = pandas.read_parquet(parquet_filepath, engine="pyarrow")
     df = df.reset_index()
     filtered_df = filter_file(df, priogrid_ids=ids, month_range_start=month_range_start,
                               month_range_end=month_range_end,
                               country_id=country_id)
-    print(filtered_df)
